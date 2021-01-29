e5b8602fbf5f874ec6ed91ed1c0a14a92a4c2661
daycd <daycd1905@gmail.com>
2021-01-29 14:51:49 +0700

    update admin portal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,9 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
+//= require jquery.min
+//= require admin
 //= require bootstrap-sprockets
-//= require jquery3
 //= require popper
 //= require bootstrap
 
@@ -12,6 +12,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "admin"
 
 Rails.start()
 Turbolinks.start()
